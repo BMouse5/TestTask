aa011f68a9c5eaf5a8656eda5244b2e1c7f71bdf
Egor <egorik1212bush@gmail.com>
2025-03-09 02:12:44 +0400

    init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <SquareComp></SquareComp>
+  </div>
+</template>
+<script setup>
+import SquareComp from './components/SquareComp.vue'
+</script>
+<style scoped>
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 80vh;
+}
+
+.square {
+  transition: background-color 0.3s;
+}
+</style>
---
+++ src/components/SquareComp.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="square" @click="switchTheme" :class="{light: isDark, dark: !isDark}"></div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const isDark = ref(false)
+
+const switchTheme = () => {
+  isDark.value = !isDark.value
+  document.body.classList.toggle('dark', isDark.value)
+}
+</script>
+
+<style>
+.square {
+    width: 20vw;
+    height: 20vw;
+    background-color: black;
+    transition: all 0.3;
+}
+
+.square.light {
+  background: white;
+}
+
+.square:hover {
+  background-color: rgb(64, 64, 64);
+}
+
+body.dark {
+  background: black;
+}
+
+</style>
